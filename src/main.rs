7f0357a4d846128ec51cc4cf6889895c6af6a8d0
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-15 03:00:50 +0000

    Send pong response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::net::TcpListener;
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
+
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,12 +15,32 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                // define a buffer 0-5
+                let mut buffer = [10; 12];
+                _stream.read_exact(&mut buffer).unwrap();
+                let command = String::from_utf8_lossy(&buffer);
+                // split command using the split method
+                let command = command.split("\r\n").collect::<Vec<&str>>();
+                let ping_command = command.last().unwrap().to_ascii_lowercase();
+                // check if the command is ping
+                if ping_command == &*"ping" {
+                    // send pong response
+                    send_pong_response(&mut _stream);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn send_pong_response(stream: &mut TcpStream) {
+    let response = "+PONG\r\n";
+    match stream.write(response.as_bytes()) {
+        Ok(_) => println!("PONG response sent"),
+        Err(e) => println!("Error while sending PONG response: {}", e),
+    }
+    stream.flush().unwrap();
 }
